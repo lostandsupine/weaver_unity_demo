6b83d1b5895a62eea9026d0ea42d823113ff03b6
aaron <aaronmaus@gmail.com>
2016-07-07 17:07:14 -0700

    organized asset file structure. Added enemy spawn manager and first enemy object

---
+++ Assets/scripts/enemies/soldier_enemy_object.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soldier_enemy_object : MonoBehaviour {
+	public Sprite[] sprite_list;
+	private soldier_enemy_object self_enemy_object;
+
+	private soldier_enemy_object (){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		sprite_list = GameObject.Find ("spawn_manager").GetComponent<spawn_manager> ().all_enemy_sprites;
+		self_enemy_object = new soldier_enemy_object ();
+
+		this.gameObject.layer = 9;
+		this.gameObject.AddComponent<SpriteRenderer> ();
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sprite_list [0];
+			
+		this.gameObject.AddComponent<BoxCollider2D> ();
+		this.gameObject.GetComponent<BoxCollider2D> ().isTrigger = false;
+
+		this.gameObject.AddComponent<Rigidbody2D> ();
+		this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
+		this.gameObject.GetComponent<Rigidbody2D> ().freezeRotation = true;
+		this.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
+
+		this.gameObject.transform.rotation = Quaternion.Euler(0,0,0);
+		this.gameObject.transform.localScale = new Vector3 (2, 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/spawn_manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn_manager : MonoBehaviour {
+	public Sprite[] all_enemy_sprites;
+
+	public void make_fireball_spell(Vector3 position_in){
+		GameObject new_soldier = new GameObject ("soldier");
+		new_soldier.tag = "enemy";
+		new_soldier.AddComponent<soldier_enemy_object> ();
+		new_soldier.transform.position = position_in;
+	}
+
+	// Use this for initialization
+	void Start () {
+		make_fireball_spell (new Vector3 (0.0f, 10.0f,-1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
