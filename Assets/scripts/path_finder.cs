b9eb710e332594096fac9fe98b99e64819d809e6
aaron <aaronmaus@gmail.com>
2016-09-24 11:40:10 -0700

    csv reader script added!

---
+++ Assets/scripts/path_finder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class path_finder : MonoBehaviour {
+
+	public TextAsset csv_file; 
+
+	void Start () {
+		string[,] grid = csv_reader.SplitCsvGrid(csv_file.text);
+		Debug.Log("size = " + (1+ grid.GetUpperBound(0)) + "," + (1 + grid.GetUpperBound(1))); 
+
+		csv_reader.DebugOutputGrid(grid); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
