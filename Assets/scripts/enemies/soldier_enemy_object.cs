5d47d4e9819dab0843e576374efd9cb19c451f56
aaron <aaronmaus@gmail.com>
2016-10-07 16:16:29 -0700

    started building soldier AI

--- Assets/scripts/enemies/soldier_enemy_object.cs
+++ Assets/scripts/enemies/soldier_enemy_object.cs
@@ -3,14 +3,53 @@ using System.Collections;
 
 public class soldier_enemy_object : MonoBehaviour {
 	public Sprite[] sprite_list;
-	int direction = 0;
+	int sprite_direction = 0;
 	int moving = 0;
 	int current_frame = 0;
-	float velocity = 3;
+	float vision_distance = 10f;
 	enum alive_state_enum {alive = 0, burning = 1, dead = 2};
 	alive_state_enum alive_state = alive_state_enum.alive;
 	float dead_time = 2f;
 	float dying_time;
+	enum ai_state_enum {
+		wandering=0,
+		fleeing=1,
+		hiding=2,
+		cowering=3,
+		seeking=4,
+		approaching=5,
+		flanking=6,
+		preparing=7,
+		charging=8
+	};
+	ai_state_enum ai_state = ai_state_enum.wandering;
+	float wander_switch_time;
+	float wander_start_time;
+	Vector2 wander_direction;
+	float wander_speed = 1f;
+	float approach_speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		wander_switch_time = 0f;
+		wander_start_time = Time.time;
+		sprite_list = GameObject.Find ("spawn_manager").GetComponent<spawn_manager> ().all_enemy_sprites;
+
+		this.gameObject.layer = 9;
+		this.gameObject.AddComponent<SpriteRenderer> ();
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sprite_list [0];
+
+		this.gameObject.AddComponent<BoxCollider2D> ();
+		this.gameObject.GetComponent<BoxCollider2D> ().isTrigger = false;
+
+		this.gameObject.AddComponent<Rigidbody2D> ();
+		this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
+		this.gameObject.GetComponent<Rigidbody2D> ().freezeRotation = true;
+		this.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
+
+		this.gameObject.transform.rotation = Quaternion.Euler(0,0,0);
+		this.gameObject.transform.localScale = new Vector3 (2, 2, 2);
+	}
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.gameObject.tag == "spell"){
@@ -23,6 +62,7 @@ public class soldier_enemy_object : MonoBehaviour {
 		this.alive_state = alive_state_enum.burning;
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sprite_list [9];
 	}
+
 	private void kill (){
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sprite_list [10];
 		this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
@@ -32,25 +72,39 @@ public class soldier_enemy_object : MonoBehaviour {
 		this.transform.Translate (new Vector3 (0, 0, 0.5f));
 	}
 
+	private float distance_to_player(){
+		return ((this.transform.position - GameObject.Find ("leila").transform.position).magnitude);
+	}
 
-	// Use this for initialization
-	void Start () {
-		sprite_list = GameObject.Find ("spawn_manager").GetComponent<spawn_manager> ().all_enemy_sprites;
+	private void do_ai()
+	{
+		switch (ai_state){
+		case ai_state_enum.wandering:
+			if (distance_to_player () <= vision_distance) {
+				ai_state = ai_state_enum.approaching;
+			}
+			break;
+		}
+	}
 
-		this.gameObject.layer = 9;
-		this.gameObject.AddComponent<SpriteRenderer> ();
-		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sprite_list [0];
-			
-		this.gameObject.AddComponent<BoxCollider2D> ();
-		this.gameObject.GetComponent<BoxCollider2D> ().isTrigger = false;
-
-		this.gameObject.AddComponent<Rigidbody2D> ();
-		this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
-		this.gameObject.GetComponent<Rigidbody2D> ().freezeRotation = true;
-		this.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
-
-		this.gameObject.transform.rotation = Quaternion.Euler(0,0,0);
-		this.gameObject.transform.localScale = new Vector3 (2, 2, 2);
+	private void ai_action(){
+		
+		switch (ai_state){
+			case ai_state_enum.wandering:
+				if ((Time.time - wander_start_time) >= wander_switch_time) {
+					wander_start_time = Time.time;
+					wander_switch_time = Random.Range (1f, 10f);
+					wander_direction = new Vector2 (Random.Range (-1f, 1f), Random.Range (-1f, 1f));
+					sprite_direction = get_sprite_direction (wander_direction);
+				}
+				this.transform.Translate (wander_direction.normalized * wander_speed * Time.deltaTime);
+				break;
+		case ai_state_enum.approaching:
+			wander_direction = GameObject.Find ("leila").transform.position - this.transform.position;
+			sprite_direction = get_sprite_direction (wander_direction);
+			this.transform.Translate (wander_direction.normalized * approach_speed * Time.deltaTime);
+			break;
+		}
 	}
 
 	public void move_soldier(int in_direction, float velocity_in){
@@ -90,64 +144,61 @@ public class soldier_enemy_object : MonoBehaviour {
 		}
 	}
 
-	public void seek_player(){
+	/*public void seek_player(){
 		Vector3 new_dir = this.transform.position - GameObject.Find ("leila").transform.position;
 		move_soldier (0, velocity * new_dir.normalized.y);
 		move_soldier (1, velocity * new_dir.normalized.x);
 
-		if (Mathf.Abs (new_dir.x) > Mathf.Abs (new_dir.y)) {
-			if (new_dir.x > 0) {
-				direction = 1;
+		sprite_direction = get_sprite_direction (new_dir);
+	}*/
+
+	private int get_sprite_direction(Vector2 direction_in){
+		int direction_out;
+		if (Mathf.Abs (direction_in.x) > Mathf.Abs (direction_in.y)) {
+			if (direction_in.x > 0) {
+				direction_out = 3;
 			} else {
-				direction = 3;
+				direction_out = 1;
 			}
 		} else {
-			if (new_dir.y > 0) {
-				direction = 0;
+			if (direction_in.y > 0) {
+				direction_out = 2;
 			} else {
-				direction = 2;
+				direction_out = 0;
 			}
+		}
+		return (direction_out);
+	}
+
+	private void choose_sprite(int direction_in){
+		GetComponent<SpriteRenderer> ().flipX = false;
+		switch (direction_in) {
+			case 0:
+				GetComponent<SpriteRenderer> ().sprite = sprite_list [1 + current_frame];
+				current_frame = (int)((3 * Time.fixedTime) % 2);
+				break;
+			case 1:
+				GetComponent<SpriteRenderer> ().sprite = sprite_list [4 + current_frame];
+				current_frame = (int)((3 * Time.fixedTime) % 2);
+				break;
+			case 2:
+				GetComponent<SpriteRenderer> ().sprite = sprite_list [7 + current_frame];
+				current_frame = (int)((3 * Time.fixedTime) % 2);
+				break;
+			case 3:
+				GetComponent<SpriteRenderer> ().sprite = sprite_list [4 + current_frame];
+				GetComponent<SpriteRenderer> ().flipX = true;
+				//this.transform.rotation = Quaternion.Euler (0, 180, 0);
+				current_frame = (int)((3 * Time.fixedTime) % 2);
+				break;
 		}
 	}
 
 	void Update () {
 		if (alive_state == alive_state_enum.alive) {
-			seek_player ();
-			GetComponent<SpriteRenderer> ().flipX = false;
-			if (1 == moving) {
-				switch (direction) {
-				case 0:
-					GetComponent<SpriteRenderer> ().sprite = sprite_list [1 + current_frame];
-					current_frame = (int)((3 * Time.fixedTime) % 2);
-					moving = 0;
-					break;
-				case 1:
-					GetComponent<SpriteRenderer> ().sprite = sprite_list [4 + current_frame];
-					current_frame = (int)((3 * Time.fixedTime) % 2);
-					moving = 0;
-					break;
-				case 2:
-					GetComponent<SpriteRenderer> ().sprite = sprite_list [7 + current_frame];
-					current_frame = (int)((3 * Time.fixedTime) % 2);
-					moving = 0;
-					break;
-				case 3:
-					GetComponent<SpriteRenderer> ().sprite = sprite_list [4 + current_frame];
-					GetComponent<SpriteRenderer> ().flipX = true;
-				//this.transform.rotation = Quaternion.Euler (0, 180, 0);
-					current_frame = (int)((3 * Time.fixedTime) % 2);
-					moving = 0;
-					break;
-				}
-
-			} else {
-				switch (direction) {
-				case 0:
-					GetComponent<SpriteRenderer> ().sprite = sprite_list [0];
-					break;
-				}
-
-			}
+			do_ai ();
+			ai_action ();
+			choose_sprite (sprite_direction);
 		} else {
 			if (alive_state == alive_state_enum.burning & (Time.time - this.dying_time) > this.dead_time) {
 				kill ();
